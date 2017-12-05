9913f21159974a9f97d045e0342ff742de065ae3
Zach Auclair <zach101@gmail.com>
2017-12-05 08:47:52 -0500

    day 5, part 1 and 2

---
+++ 2017/day-05/solution.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("./input.txt")
+	if err != nil {
+		panic(fmt.Errorf("Failed to open input: %s", err))
+	}
+	defer f.Close()
+	s := bufio.NewScanner(f)
+	list := []int{}
+	for s.Scan() {
+		nextInt, err := strconv.Atoi(s.Text())
+		if err != nil {
+			panic(fmt.Errorf("Failed to parse %s: %s", s.Text(), err))
+		}
+		list = append(list, nextInt)
+	}
+
+	var moves uint64
+	for index := 0; index < len(list) && index > -1; {
+		atIndex := list[index]
+		if atIndex >= 3 {
+			list[index]--
+		} else {
+			list[index]++
+		}
+		index += atIndex
+		moves++
+	}
+
+	if err := s.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("moves: %d\n", moves)
+}
