b0333db6d2c31b56811e06f315cf74cd22f32646
Zach Auclair <zach101@gmail.com>
2017-12-06 00:20:12 -0500

    day 6, part 1

---
+++ 2017/day-06/solution.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var data = `4	1	15	12	0	9	9	5	5	8	7	3	14	5	12	3`
+
+//var data = `0 2 7 0`
+
+func main() {
+	spaceRe := regexp.MustCompile("\\s+")
+	parts := spaceRe.Split(data, -1)
+	v := []int{}
+	// ideally, use a heap to keep track of the highest count index
+	for _, p := range parts {
+		val, err := strconv.Atoi(p)
+		if err != nil {
+			panic(err)
+		}
+		v = append(v, val)
+	}
+	//fmt.Printf("%v\n", v)
+
+	valCount := len(v)
+	seen := map[string]interface{}{}
+	var iterations uint64
+	for {
+		// find max
+		var max int
+		var index int
+		for i, val := range v {
+			if val > max {
+				max = val
+				index = i
+			}
+		}
+
+		// distribute max's blocks in a clockwise fashion
+		v[index] = 0
+		for ; max > 0; max-- {
+			index = (index + 1) % valCount
+			v[index]++
+		}
+
+		// serialize our iteration's state or bail
+		s := []string{}
+		for _, v := range v {
+			s = append(s, strconv.Itoa(v))
+		}
+		s2 := strings.Join(s, ",")
+		if _, ok := seen[s2]; ok {
+			iterations++
+			break
+		}
+
+		//fmt.Printf("%s\n", s2)
+		seen[s2] = struct{}{}
+		iterations++
+	}
+
+	// 6681
+	fmt.Printf("Iterations: %d\n", iterations)
+}
