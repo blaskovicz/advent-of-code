dcf7f35f9951dd872a71d343756e1e0b4e9b1346
Zach Auclair <zach101@gmail.com>
2017-12-10 17:23:45 -0500

    day 10, part 2

--- 2017/day-10/solution.go
+++ 2017/day-10/solution.go
@@ -1,47 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type solver struct {
+	inputIndex int
+	skip       int
 }
 
 func reverse(input []int, inputLen, startIndex, count int) {
-	//fmt.Printf("input.rotate=%#v, rotate=%d, index=%d\n", input, count, startIndex)
 	for i := 0; i < count/2; i++ {
 		left := (startIndex + i) % inputLen
 		right := (startIndex + count - 1 - i) % inputLen
 		input[left], input[right] = input[right], input[left]
-		//fmt.Printf("\t[%d] %#v\n", i, input)
 	}
 }
 
-func (s *solver) knotHash(input, lengths []int) {
-	inputIndex := 0
-	inputLen := len(input)
-	var skip int
-	for _, length := range lengths {
-		reverse(input, inputLen, inputIndex, length)
-		inputIndex = inputIndex + (length+skip)%inputLen
-		if inputIndex >= inputLen {
-			inputIndex = inputIndex % inputLen
-		}
+func (s *solver) denseKnotHash(data string) string {
+	lengths := asciiData(data)
 
-		skip++
-		//fmt.Printf("\tinput.next=%#v, skip=%d, index.next=%d\n\n", input, skip, inputIndex)
+	input := make([]int, 256, 256)
+	for i := 0; i < 256; i++ {
+		input[i] = i
 	}
+	for i := 0; i < 64; i++ {
+		s.knotHash(input, lengths)
+	}
+
+	var denseHash string
+	for i := 0; i < 16; i++ {
+		offset := i * 16
+		end := offset + 16
+		denseHash += fmt.Sprintf("%02x", xorBlock(input[offset:end]))
+	}
+	return denseHash
+}
+
+func (s *solver) knotHash(input, lengths []int) {
+	inputLen := len(input)
+	for _, length := range lengths {
+		reverse(input, inputLen, s.inputIndex, length)
+		s.inputIndex = s.inputIndex + (length+s.skip)%inputLen
+		if s.inputIndex >= inputLen {
+			s.inputIndex = s.inputIndex % inputLen
+		}
+		s.skip++
+	}
+}
+
+func xorBlock(input []int) int {
+	result := input[0]
+	for i := 1; i < len(input); i++ {
+		result ^= input[i]
+	}
+	return result
 }
 
 func newSolver() *solver {
 	return &solver{}
 }
 
-var lengths = []int{227, 169, 3, 166, 246, 201, 0, 47, 1, 255, 2, 254, 96, 3, 97, 144}
+//var lengths = []int{227, 169, 3, 166, 246, 201, 0, 47, 1, 255, 2, 254, 96, 3, 97, 144}
+var data = "227,169,3,166,246,201,0,47,1,255,2,254,96,3,97,144"
 
-func main() {
-	list := make([]int, 256, 256)
-	for i := 0; i < 256; i++ {
-		list[i] = i
+func asciiData(s string) []int {
+	lengths := []int{}
+	for _, b := range []byte(s) {
+		lengths = append(lengths, int(b))
 	}
-	newSolver().knotHash(list, lengths)
-	fmt.Printf("answer: %d\n", list[0]*list[1])
+	lengths = append(lengths, 17, 31, 73, 47, 23)
+	return lengths
+}
+func main() {
+	fmt.Printf("Answer: %s\n", newSolver().denseKnotHash(data))
 }
--- 2017/day-10/solution_test.go
+++ 2017/day-10/solution_test.go
@@ -12,6 +12,11 @@ type testInput struct {
 	count   int
 }
 
+type denseTestInput struct {
+	input  string
+	result string
+}
+
 func TestSolution(t *testing.T) {
 	for _, ti := range []testInput{
 		testInput{[]int{0, 1, 2, 3, 4}, []int{3, 4, 1, 5}, 12},
@@ -22,5 +27,17 @@ func TestSolution(t *testing.T) {
 		s.knotHash(ti.input, ti.lengths)
 		actualCount := ti.input[0] * ti.input[1]
 		require.Equal(t, ti.count, actualCount, "Multiplication of first 2 result numbers incorrect")
+	}
+
+	for _, ti := range []denseTestInput{
+		denseTestInput{"", "a2582a3a0e66e6e86e3812dcb672a272"},
+		denseTestInput{"AoC 2017", "33efeb34ea91902bb2f59c9920caa6cd"},
+		denseTestInput{"1,2,3", "3efbe78a8d82f29979031a4aa0b16a9d"},
+		denseTestInput{"1,2,4", "63960835bcdc130f0b66d7ff4f6a5a8e"},
+	} {
+		s := newSolver()
+		require.NotNil(t, s, "solver didn't get instantiated")
+		actual := s.denseKnotHash(ti.input)
+		require.Equal(t, ti.result, actual, "Dense hash incorrect")
 	}
 }
