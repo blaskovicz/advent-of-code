9ee8eff685f1c341842f630edbb2eb03b5e2e670
Zach Auclair <zach101@gmail.com>
2017-12-17 12:13:18 -0500

    day 17, part 2

--- 2017/day-17/solution.go
+++ 2017/day-17/solution.go
@@ -12,8 +12,30 @@ var steps = 356
 //var steps = 3
 
 func main() {
-	target := 2017
-	head := &node{0, nil}
+	target := 50000000
+	currentPosition := 0
+	afterZero := -1
+	for i := 1; i <= target; i++ {
+		// walk forward n steps from current position, normalized to array len
+		walk := (currentPosition + steps) % i
+
+		// inserting at index 0
+		if walk == 0 {
+			afterZero = i
+		}
+
+		// update current position to newly inserted node index
+		currentPosition = walk + 1
+	}
+	fmt.Printf("After zero: %d\n", afterZero)
+}
+
+func main1() {
+	//target := 2017
+	target := 50000000
+	zero := &node{0, nil}
+	lastZero := 0
+	head := zero
 	current := head
 	nextNum := 1
 	for nextNum <= target {
@@ -35,11 +57,15 @@ func main() {
 		// update insert position to node
 		current = newNode
 		nextNum++
+		// if lastZero != zero.next.data {
+		// 	fmt.Printf("[%d] -> %d\n", nextNum-1, zero.next.data)
+		// 	lastZero = zero.next.data
+		// }
 	}
 
 	// print result
-	if current.next != nil {
-		fmt.Printf("%d\n", current.next.data)
+	if zero.next != nil {
+		fmt.Printf("%d\n", zero.next.data)
 	} else {
 		fmt.Printf("%d\n", head.data)
 	}
